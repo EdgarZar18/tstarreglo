3d0229db980b03e5c674fbf2258140162bd099b7
edgar zarate <edgar.zp92@gmail.com>
2020-07-22 18:29:28 -0500

    quienes somos added

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+/**
+ * cliente: nombre, direccion, ciudad, numero de telefono
+ * Servicios: estos pueden ser de garantias o bajo costo
+ * Cotizaciones: preguntar por piezas o equipo que se llegue a necesitar
+ * tiempo de reparacion: cuanto tiempo tiene el aparato en la tienda
+ * estatus de los servicios: estan en espera de pizas para reparacion, respuesta de proveedor,
+ */
+
+var Cliente = function(nombreCompleto, direccionCompleta, ciudadResidencia, numeroTelefono) {
+
+}
+
+ var Servicios = function(tipo,falla,estatus,comentario) {
+
+ }
+
+ var Cotizaciones = function(conceptoProdServ, descripcionConcepto, precioConcepto) {
+
+ }
+
+ 
